8eb50f8d060198c2f67246a66597f72d7f83b940
mromaris <45624401+mromaris@users.noreply.github.com>
2020-04-03 13:04:31 +0200

    Update users.py
    Made de code cleaner storing the data into a variable. Also added an if that checks if there is any data in case we scrape a wrong user or in an invalid format from tikrank.

--- tiktok/tiktok/spiders/users.py
+++ tiktok/tiktok/spiders/users.py
@@ -9,13 +9,17 @@ class UsersSpider(scrapy.Spider):
 	custom_settings = {'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
 
 	def parse(self, response):
-		yield {
-			'ChannelName': json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']['userData']['uniqueId'],
-			'Nickname': json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']['userData']['nickName'],
-			'FollowingCount': json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']['userData']['following'],
-			'FollowerCount': json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']['userData']['fans'],
-			'ChannelDescription': json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']['userData']['signature'],
-			'VideoCount' : json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']['userData']['video'],
-			'TotalLikes' : json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']['userData']['heart'],
-			'Verified' : json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']['userData']['verified'],
-		}
+		data = json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']
+		if 'userData' not in data:
+		    yield None
+		else:
+		    yield {
+			'ChannelName': data['userData']['uniqueId'],
+			'Nickname':  data['userData']['nickName'],
+			'FollowingCount':  data['userData']['following'],
+			'FollowerCount':  data['userData']['fans'],
+			'ChannelDescription':  data['userData']['signature'],
+			'VideoCount' :  data['userData']['video'],
+			'TotalLikes' :  data['userData']['heart'],
+			'Verified' :  data['userData']['verified'],
+		    }
