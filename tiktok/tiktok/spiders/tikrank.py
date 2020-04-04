a6229a72773f0170b84e90b7779fcce551e918e7
Nicole Loupias <loupiasnicole@gmail.com>
2020-04-04 17:14:04 +0200

    Crawler done
    Modified users.py so that scrapy gets the users from TikRank of Spain and Germany (20000 users) and gets the information of those users in Tiktok.
    Also modified the settings.py to not get banned from Tiktok because of doing many requests.

--- tiktok/tiktok/spiders/tikrank.py
+++ tiktok/tiktok/spiders/tikrank.py
@@ -5,13 +5,15 @@ import json
 
 class TikrankSpider(scrapy.Spider):
     name = 'tikrank'
-    start_urls = ['https://tikrank.com/influencer/influencers?page_num=1&page_size=10&sorted_by=followers&country=DE&fans_count=0&keyword=&order=desc']
+    start_urls = ['https://tikrank.com/influencer/influencers?page_num=1&page_size=10000&sorted_by=followers&country=DE&fans_count=0&keyword=&order=desc']
     
     def parse(self, response):
         people = response.text
         parsed = json.loads(people)["data"]["kols"]
+        i = 0
         for person in parsed:
-            print(person["kol_unique_id"])
+            i = i + 1
+            print(i ," ", person["kol_unique_id"])
         yield None
 
 
--- tiktok/tiktok/spiders/users.py
+++ tiktok/tiktok/spiders/users.py
@@ -3,23 +3,33 @@ import scrapy
 import json
 
 class UsersSpider(scrapy.Spider):
-	name = 'users'
-	allowed_domains = ['tiktok.com']
-	start_urls = ['https://www.tiktok.com/@andragogan']
-	custom_settings = {'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
-
-	def parse(self, response):
-		data = json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']
-		if 'userData' not in data:
-		    yield None
-		else:
-		    yield {
-			'ChannelName': data['userData']['uniqueId'],
-			'Nickname':  data['userData']['nickName'],
-			'FollowingCount':  data['userData']['following'],
-			'FollowerCount':  data['userData']['fans'],
-			'ChannelDescription':  data['userData']['signature'],
-			'VideoCount' :  data['userData']['video'],
-			'TotalLikes' :  data['userData']['heart'],
-			'Verified' :  data['userData']['verified'],
-		    }
+    name = 'users'
+    allowed_domains = ['tiktok.com']
+    start_urls = ['https://tikrank.com/influencer/influencers?page_num=10000&page_size=1&sorted_by=followers&country=FR&fans_count=0&keyword=&order=desc', 'https://tikrank.com/influencer/influencers?page_num=1&page_size=10000&sorted_by=followers&country=ES&fans_count=0&keyword=&order=desc']
+    custom_settings = {'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
+    tiktok_url = 'https://www.tiktok.com/@'
+    def parse(self, response):
+        people = response.text
+        parsed = json.loads(people)["data"]["kols"]
+        for person in parsed:
+            yield scrapy.Request(url = self.tiktok_url + person["kol_unique_id"], callback = self.parse_users, meta = { "country" : response.request.url })
+    def parse_users(self, response):
+        data = json.loads(response.css('script#__NEXT_DATA__::text')[0].re('.*')[0])['props']['pageProps']
+        if 'userData' not in data:
+            yield None
+        else:
+            if response.meta['country'] == 'https://tikrank.com/influencer/influencers?page_num=1&page_size=1&sorted_by=followers&country=DE&fans_count=0&keyword=&order=desc':
+                country = 'Germany'
+            else:
+                country = 'Spain'
+            yield {
+                'ChannelName': data['userData']['uniqueId'],
+                'Nickname':  data['userData']['nickName'],
+                'FollowingCount':  data['userData']['following'],
+                'FollowerCount':  data['userData']['fans'],
+                'ChannelDescription':  data['userData']['signature'],
+                'VideoCount' :  data['userData']['video'],
+                'TotalLikes' :  data['userData']['heart'],
+                'Verified' :  data['userData']['verified'],
+                'Country': country
+            }
