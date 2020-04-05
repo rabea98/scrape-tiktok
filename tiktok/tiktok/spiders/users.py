980a619f2c92eec74ad3d0799bcd0a0016d64874
mromaris <45624401+mromaris@users.noreply.github.com>
2020-04-05 12:36:14 +0200

    Fixed Countries
    Changed FR to DE

--- tiktok/tiktok/spiders/users.py
+++ tiktok/tiktok/spiders/users.py
@@ -5,7 +5,7 @@ import json
 class UsersSpider(scrapy.Spider):
     name = 'users'
     allowed_domains = ['tiktok.com']
-    start_urls = ['https://tikrank.com/influencer/influencers?page_num=10000&page_size=1&sorted_by=followers&country=FR&fans_count=0&keyword=&order=desc', 'https://tikrank.com/influencer/influencers?page_num=1&page_size=10000&sorted_by=followers&country=ES&fans_count=0&keyword=&order=desc']
+    start_urls = ['https://tikrank.com/influencer/influencers?page_num=1&page_size=10000&sorted_by=followers&country=DE&fans_count=0&keyword=&order=desc', 'https://tikrank.com/influencer/influencers?page_num=1&page_size=10000&sorted_by=followers&country=ES&fans_count=0&keyword=&order=desc']
     custom_settings = {'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
     tiktok_url = 'https://www.tiktok.com/@'
     def parse(self, response):
